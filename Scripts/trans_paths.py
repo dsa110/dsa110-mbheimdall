ec8a258b86f519800bc8331c1ee96ed2aaedcf87
ubuntu <ubuntu@pm.me>
2021-03-10 18:18:24 +0000

    small cleanup

--- Scripts/trans_paths.py
+++ Scripts/trans_paths.py
@@ -5,11 +5,11 @@
 #
 
 def getControlDir():
-  return "/home/dsa/dsa110-mbheimdall/control"
+  return "/home/ubuntu/proj/dsa110-shell/dsa110-mbheimdall/control"
 
 def getConfigDir():
-  return "/home/dsa/dsa110-mbheimdall/share"
+  return "/home/ubuntu/proj/dsa110-shell/dsa110-mbheimdall/share"
 
 def getBinaryDir():
-  return "/home/dsa/dsa110-mbheimdall/bin"
+  return "/home/ubuntu/proj/dsa110-shell/dsa110-mbheimdall/bin"
 
